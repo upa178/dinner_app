88877894316faac62ae9cc7b7aad5f3df428b07c
upa178 <u.chanchan0910@gmail.com>
2022-04-01 15:13:22 +0900

    add: composer.jsonファイルを作成し、composerをインストールした。

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'b55a4b7c8a99509cbd017199a7b567303265002e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'b55a4b7c8a99509cbd017199a7b567303265002e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
