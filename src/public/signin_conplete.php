ba6846bec338ae41c4a4c031552e22e1d0c9dc3e
upa178 <u.chanchan0910@gmail.com>
2022-04-04 09:40:52 +0900

    feat: サインイン時のバリデーション処理の追加

---
+++ src/public/signin_conplete.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Lib\Redirect;
+use App\Domain\Infrastructure\Validator\SigninInputValidator;
+
+$email = filter_input(INPUT_POST, 'email');
+$password = filter_input(INPUT_POST, 'password');
+
+try {
+    $signinInputError = new SignInInputValidator($email, $password);
+    if (!empty($errors)) {
+        $session->setErrors($errors);
+        Redirect::handler('/signin.php');
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+    die();
+}
