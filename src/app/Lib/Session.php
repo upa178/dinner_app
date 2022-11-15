86ace4251641900cf8501dc630fdd6db62cd3f53
upa178 <u.chanchan0910@gmail.com>
2022-11-15 22:24:48 +0900

    feat: Session登録処理の追加

--- src/app/Lib/Session.php
+++ src/app/Lib/Session.php
@@ -2,9 +2,21 @@
 
 namespace App\Lib;
 
+use App\Domain\ValueObject\AuthUser;
+use App\Domain\ValueObject\Email;
+use App\Domain\ValueObject\Name;
+use App\Domain\ValueObject\UserId;
+
 final class Session
 {
+    const ERRORS = 'errors';
     const AUTH_KEY = 'authKey';
+
+    const USER_KEY = 'userKey';
+
+    const NAME_KEY = 'nameKey';
+
+    const EMAIL_KEY = 'emailKey';
     private static $instance;
     public static function getInstance()
     {
@@ -24,7 +36,51 @@ final class Session
         }
     }
 
-    public function setUserId()
+    public function auth(): ?array
     {
+        return $_SESSION[self::AUTH_KEY] ?? null;
+    }
+
+
+    public function setErrors(array $errors): array
+    {
+        return $_SESSION[self::ERRORS] = $errors;
+    }
+
+    /**
+     * セッションに保存したエラー情報を返す
+     * 存在しなければnullを返す
+     * @return array
+     */
+    public function errors(): array
+    {
+        return $_SESSION[self::ERRORS] ?? [];
+    }
+
+    public function clearErrors(): void
+    {
+        unset($_SESSION[self::ERRORS]);
+    }
+
+    public function setAuth(AuthUser $authUser)
+    {
+        $this->setUserId($authUser->userId());
+        $this->setUserName($authUser->userName());
+        $this->setUserEmail($authUser->email());
+    }
+
+    private function setUserId(UserId $userId): void
+    {
+        $_SESSION[self::USER_KEY] = $userId->value();
+    }
+
+    private function setUserName(Name $name): void
+    {
+        $_SESSION[self::NAME_KEY] = $name->value();
+    }
+
+    private function setUserEmail(Email $email): void
+    {
+        $_SESSION[self::EMAIL_KEY] = $email->value();
     }
 }
